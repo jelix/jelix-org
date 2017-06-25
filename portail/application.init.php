5e3e36f8ff2bb30e09fa51185a0d3a8e6c31e017
Laurent Jouanneau <dev@ljouanneau.com>
2017-06-25 21:17:39 +0200

    Use env variables for temp & log path

--- portail/application.init.php
+++ portail/application.init.php
@@ -6,13 +6,14 @@
  * @link     http://jelix.org
  * @licence  http://www.gnu.org/licenses/gpl.html GNU General Public Licence, see LICENCE file
  */
-require(dirname(__FILE__).'/../lib/jelix/init.php');
-
 define ('JELIX_APP_PATH', dirname (__FILE__).'/'); // don't change
 
-define ('JELIX_APP_TEMP_PATH',    realpath(JELIX_APP_PATH.'../temp/portail/').'/');
+require(JELIX_APP_PATH.'../lib/jelix/init.php');
+
+define ('JELIX_APP_TEMP_PATH',    getenv('WWW_JELIX_ORG_TEMP_PATH'));
+define ('JELIX_APP_LOG_PATH',     getenv('WWW_JELIX_ORG_LOG_PATH'));
+
 define ('JELIX_APP_VAR_PATH',     JELIX_APP_PATH.'var/');
-define ('JELIX_APP_LOG_PATH',     JELIX_APP_PATH.'var/log/');
 define ('JELIX_APP_CONFIG_PATH',  JELIX_APP_PATH.'var/config/');
 define ('JELIX_APP_WWW_PATH',     realpath(JELIX_APP_PATH.'../www/').'/');
 define ('JELIX_APP_CMD_PATH',     JELIX_APP_PATH.'scripts/');
--- portail/jelix-scripts.init.php
+++ portail/jelix-scripts.init.php
@@ -6,22 +6,22 @@
  * @link     http://jelix.org
  * @licence  http://www.gnu.org/licenses/gpl.html GNU General Public Licence, see LICENCE file
  */
-
-require(dirname(__FILE__).'/../lib/jelix/init.php');
-
 define ('JELIX_APP_PATH', dirname (__FILE__).'/'); // don't change
 
+require(JELIX_APP_PATH.'../lib/jelix/init.php');
+
+define ('JELIX_APP_LOG_PATH',     getenv('WWW_JELIX_ORG_LOG_PATH'));
+
 define ('JELIX_APP_VAR_PATH',     JELIX_APP_PATH.'var/');
-define ('JELIX_APP_LOG_PATH',     JELIX_APP_PATH.'var/log/');
 define ('JELIX_APP_CONFIG_PATH',  JELIX_APP_PATH.'var/config/');
 define ('JELIX_APP_WWW_PATH',     realpath(JELIX_APP_PATH.'../www/').'/');
 define ('JELIX_APP_CMD_PATH',     JELIX_APP_PATH.'scripts/');
 
 // the temp path for jelix-scripts
-define ('JELIX_APP_TEMP_PATH',    realpath(JELIX_APP_PATH.'../temp/portail-scripts/').'/');
+define ('JELIX_APP_TEMP_PATH',    getenv('WWW_JELIX_ORG_TEMP_SCRIPTS_PATH'));
 
 // the temp path for cli scripts of the application
-define ('JELIX_APP_TEMP_CLI_PATH',    realpath(JELIX_APP_PATH.'../temp/portail-cli/').'/');
+define ('JELIX_APP_TEMP_CLI_PATH',    getenv('WWW_JELIX_ORG_TEMP_CLI_PATH'));
 
 // the temp path for the web scripts of the application (the same value as JELIX_APP_TEMP_PATH in application.init.php)
-define ('JELIX_APP_REAL_TEMP_PATH',    realpath(JELIX_APP_PATH.'../temp/portail/').'/');
+define ('JELIX_APP_REAL_TEMP_PATH',    getenv('WWW_JELIX_ORG_TEMP_PATH'));
