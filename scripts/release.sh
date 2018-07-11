758c9cb58816bce49f71be37e1dd8e6afa5c87e9
Laurent Jouanneau <dev@ljouanneau.com>
2018-07-11 15:23:46 +0200

    release jelix 1.6.18

--- scripts/release.sh
+++ scripts/release.sh
@@ -92,17 +92,17 @@ $SCRIPTDIR/build_release_doc $BRANCH_VERSION $TAG_NAME $VERSION
 #mv $DIR_JELIX_SITES/../docs.jelix.org/books/pdf/en/jelix-$MANUAL_EN.pdf $SCRIPTDIR/../download/www/jelix/releases/${VER_MAJOR}.${VER_MIDDLE}.x/$VERSION/jelix-manual-$VERSION.pdf
 #mv $DIR_JELIX_SITES/../docs.jelix.org/books/pdf/fr/jelix-$MANUAL_FR.pdf $SCRIPTDIR/../download/www/jelix/releases/${VER_MAJOR}.${VER_MIDDLE}.x/$VERSION/jelix-manuel-$VERSION.pdf
 
-echo "write version into the latest-stable-version file"
-API_DIR="$SCRIPTDIR/../www/api/releases/${VER_MAJOR}.${VER_MIDDLE}"
-if [ ! -d $API_DIR ]
-then
-    mkdir $API_DIR
-fi
-echo $VERSION > "$API_DIR/latest-stable-version"
+#echo "write version into the latest-stable-version file"
+#API_DIR="$SCRIPTDIR/../www/api/releases/${VER_MAJOR}.${VER_MIDDLE}"
+#if [ ! -d $API_DIR ]
+#then
+#    mkdir $API_DIR
+#fi
+#echo $VERSION > "$API_DIR/latest-stable-version"
 
-echo "clear wiki cache"
+#echo "clear wiki cache"
 
-rm -rf $DIR_JELIX_SITES/www/data/cache/*
+#rm -rf $DIR_JELIX_SITES/www/data/cache/*
 
 echo ""
 echo "End of the release!"
